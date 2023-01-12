7b66415701179106c076b1fbbeb63cd900a224ed
Leonardo-Cardoso-ie <leonardocardoso.ie@gmail.com>
2023-01-12 11:06:57 +0000

    Add variables and test the terminal

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Minimum number
+# Create a variable for a minimum number and insert input
+min_number = input('Please enter minimum number: ')
+ 
+# Maximum number 
+# Create a variable for the maximum number and insert input
+max_number = input('Please enter maximum number: ')
+
+# Random number to guess
+
+# NUmber of chances
+
+# Tell us if we guessed to high or low
+
+# Tell us if we are right or wrong
+
+# Prompt us if win or loose
+
+# Keep score and give us an option to retry
